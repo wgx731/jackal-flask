baa0425e5226c180120de1b621d30c14633c81e5
wgx731 <wgx731@gmail.com>
2017-09-19 02:12:02 +0800

    test[rest]: test stocks api

--- tests.py
+++ tests.py
@@ -24,6 +24,42 @@ class JackalFlaskTest(unittest.TestCase):
         self.assertIn(b'GOOGL', result.data)
         self.assertIn(b'AAPL', result.data)
 
+    def test_stocks_csv(self):
+        """Assert that stock csv api returns csv result"""
+        result = self.app.get('/api/stocks.csv')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('text/csv', result.headers['Content-Type'])
+        self.assertIn(b'GOOGL', result.data)
+        self.assertIn(b'AAPL', result.data)
+
+    def test_stocks_txt(self):
+        """Assert that stock txt api returns txt result"""
+        result = self.app.get('/api/stocks.txt')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('text/plain', result.headers['Content-Type'])
+        self.assertIn(b'GOOGL', result.data)
+        self.assertIn(b'AAPL', result.data)
+
+    def test_stocks_json(self):
+        """Assert that stock json api returns json result"""
+        result = self.app.get('/api/stocks.json')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('application/json', result.headers['Content-Type'])
+        self.assertIn(b'GOOGL', result.data)
+        self.assertIn(b'AAPL', result.data)
+
+    def test_stocks(self):
+        """Assert that stock json api returns json result"""
+        result = self.app.get('/api/stocks', headers={'Accept': 'text/csv'})
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('text/csv', result.headers['Content-Type'])
+        result = self.app.get('/api/stocks', headers={'Accept': 'text/plain'})
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('text/plain', result.headers['Content-Type'])
+        result = self.app.get('/api/stocks', headers={'Accept': 'application/json'})
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('application/json', result.headers['Content-Type'])
+
 if __name__ == '__main__':
     unittest.main()
 

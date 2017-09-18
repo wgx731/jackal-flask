7316ddd15f297a5c6dd050c7d63564bb6661b405
wgx731 <wgx731@gmail.com>
2017-09-18 23:52:14 +0800

    test[mvc]: update test

--- tests.py
+++ tests.py
@@ -11,11 +11,18 @@ class JackalFlaskTest(unittest.TestCase):
     def tearDown(self):
         del self.app
 
-    def test_home(self):
-        """Assert that user successfully lands on home page"""
+    def test_index(self):
+        """Assert that user successfully lands on index page"""
         result = self.app.get('/index')
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'wgx731', result.data)
+
+    def test_home(self):
+        """Assert that user successfully lands on home page"""
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'GOOGL', result.data)
+        self.assertIn(b'AAPL', result.data)
 
 if __name__ == '__main__':
     unittest.main()

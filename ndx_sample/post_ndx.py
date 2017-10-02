db5a76325c4b67c35375ec108561a0d45a41ddc1
wgx731 <wgx731@gmail.com>
2017-10-02 22:32:46 +0800

    fix[final]: post data script

--- ndx_sample/post_ndx.py
+++ ndx_sample/post_ndx.py
@@ -3,33 +3,42 @@ import sys
 import requests
 import random
 import json
-import time
-from urlparse import urlparse
+from urllib.parse import urlparse
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print("wrong number of args.")
         sys.exit(1)
     pr = urlparse(sys.argv[1])
     if pr[0] == '' or pr[1] == '':
         print("wrong base url given.")
         sys.exit(2)
+    token = sys.argv[2]
+    r = requests.get(
+        '{}/api/stocks'.format(sys.argv[1]),
+        headers={
+            'Authorization': 'Bearer ' + token
+        }
+    )
+    if (r.status_code != 200):
+        print("wrong token given.")
+        sys.exit(3)
     symbol_list = []
-    with open('NASDAQ.csv', mode='rb') as infile:
+    with open('NASDAQ.csv', mode='r') as infile:
         reader = csv.DictReader(infile)
         for line in reader:
             symbol_list.append(line['Symbol'].strip())
-    with open('NYSE.csv', mode='rb') as infile:
+    with open('NYSE.csv', mode='r') as infile:
         reader = csv.DictReader(infile)
         for line in reader:
             symbol_list.append(line['Symbol'].strip())
-    with open('ASX.csv', mode='rb') as infile:
+    with open('ASX.csv', mode='r') as infile:
         reader = csv.DictReader(infile)
         for line in reader:
             symbol_list.append(line['ASX code'].strip())
     symbol_list = list(set(symbol_list))
     print('Total Number Of Symbol: {}'.format(len(symbol_list)))
-    with open('ndx.csv', mode='rb') as infile:
+    with open('ndx.csv', mode='r') as infile:
         reader = csv.DictReader(infile)
         for line in reader:
             line.pop('oi', None)
@@ -38,8 +47,10 @@ if __name__ == "__main__":
             r = requests.post(
                 '{}/api/stock'.format(sys.argv[1]),
                 data=json.dumps(line),
-                headers={'content-type': 'application/json'}
+                headers={
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                }
             )
             print(r.status_code)
             print(r.text)
-
